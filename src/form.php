051245b2e3b2f90f9dd944199d0d4c093301ecdd
nunkmaia <carmocovelomaiara@gmail.com>
2026-03-19 14:40:50 +0000

    19/03/2026

---
+++ src/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</Form></title>
+</head>
+<body>
+    <h1> Form - 19/03/2026 </h1>
+    <form method="post" action="recebe.php">
+        Login <input type="text" id="login" name="login"><br>
+        Senha <input type="password" id="senha" name="senha"><br>
+        <input type="submit" value="Validar">
+
+    </form>
+</body>
+</html>
